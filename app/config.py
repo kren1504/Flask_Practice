af1a759325618f08685b1333d62afe8d9b4e0ecd
Karen Gonzalez <kren1504@hotmail.com>
2021-01-20 15:10:45 -0400

    primer commit clase nro 28 flask

---
+++ app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SECRET_KEY = 'SUPER SECRETE'
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+import unittest
+
+from app import create_app
+from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
+
+app = create_app()
+
+
+#todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+
+    return response
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    user_ip = session.get('user_ip')
+    username = session.get('username')
+
+    context = {
+        'user_ip': user_ip,
+        'todos': get_todos(user_id=username),
+        'username': username
+    }
+
+    users = get_users()
+
+    for user in users:
+        print(user.id)
+        print(user.to_dict()['password'])
+
+
+    return render_template('hello.html', **context)
